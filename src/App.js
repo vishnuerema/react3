873974ec6d3745569b3dd0e623c17aba451ea27d
Vishnue <1234554321tamizhan@gmail.com>
2019-11-23 03:13:56 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Header from './components/headercomponent/Header';
+import Footer from './components/footercomponent/Footer';
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import './assets/css/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    
+      return (
+        <Router>
+          <div className ="App">
+            <Header />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/About" component={About} />
+                <Route exact path="/Contact" component={Contact} />
+            <Footer />
+          </div>
+        </Router>
+      );
+  }
 }
 
 export default App;
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Contact extends Component {
+    render(){
+        return(
+                <div className ="contact">
+                   <p className ="heading_home">Contact</p>
+                    <p className ="heading_sub">
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+        )
+    }
+}
+
+export default Contact;
